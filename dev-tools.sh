7f166c3bde37afd17440f6cac9ccde42a7fbe7ca
Guy Owen <guy.owen@dpc.vic.gov.au>
2019-11-13 15:04:57 +1100

    Updated Dev-tools commit pointer.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,6 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=96ae41bcdda3becfda4552d858025dc69799ff67
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
