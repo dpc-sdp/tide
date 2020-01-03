eed654a7b74093bf9cfe4eed972798c46e2e2739
Anthony Malkoun <anthony.malkoun@dpc.vic.gov.au>
2020-01-03 17:29:58 +1100

    Use tagged versions of tide_core and tide_api

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,5 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=dcc4a3d1b3aef9134eab17a49a4ebe70edc0a9c3
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
