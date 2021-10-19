e6491e54405adabfa35ad53485dbc4832c93de42
MdNadimHossain <getovic@gmail.com>
2021-10-20 10:51:17 +1100

    Updated tide contrib modules to D9 version.

--- dev-tools.sh
+++ dev-tools.sh
@@ -33,7 +33,6 @@
 # stable version.
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
-# export GH_COMMIT=a246dd0ddce06481c38f952f9286cda0b31a9d05
-export GH_COMMIT=a246dd0ddce06481c38f952f9286cda0b31a9d05
+# export GH_COMMIT=COMMIT_SHA
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
