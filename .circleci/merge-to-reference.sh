6da13c0c396f8e5f317df045f16343b1b9d6a1a3
Keith Lau <keith.lau@dpc.vic.gov.au>
2022-11-18 11:37:03 +1100

    added script

--- .circleci/merge-to-reference.sh
+++ .circleci/merge-to-reference.sh
@@ -7,8 +7,10 @@ echo "==> Checking out reference branch"
 git checkout reference
 echo "==> Merging develop to reference branch"
 git merge develop
-echo "==> Making sure composer.json is unchanged"
-git checkout composer.json
+echo "==> Replacing composer require entries starting with dpc-sdp with value dev-reference"
+cat composer.json | jq '.require |= with_entries(
+  if (.key | test("dpc-sdp/tide"))
+  then .value = "dev-reference" else . end)' > composer.json.backup && mv -f composer.json.backup composer.json
 echo "==> Add all changes"
 git add .
 git commit -m "Merge changes from develop."
