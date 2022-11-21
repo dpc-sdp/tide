b805b012627d4fcd87bceef9da49ade88dc1b65a
Md Nadim Hossain <getovic@gmail.com>
2022-11-21 14:33:54 +1100

    Updated test script.

--- .circleci/merge-to-reference.sh
+++ .circleci/merge-to-reference.sh
@@ -7,10 +7,11 @@ echo "==> Checking out reference branch"
 git checkout reference
 echo "==> Merging develop to reference branch"
 git merge develop2
+git checkout --theirs composer.json
 echo "==> Replacing composer require entries starting with dpc-sdp with value dev-reference"
-# cat composer.json | jq '.require |= with_entries(
-#   if (.key | test("dpc-sdp/tide"))
-#   then .value = "dev-reference" else . end)' > composer.json.backup && mv -f composer.json.backup composer.json
+cat composer.json | jq '.require |= with_entries(
+  if (.key | test("dpc-sdp/tide"))
+  then .value = "dev-reference" else . end)' > composer.json.backup && mv -f composer.json.backup composer.json
 echo "==> Add all changes"
 git add .
 git commit -m "Merge changes from develop."
