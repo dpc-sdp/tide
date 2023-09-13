8ce61670ffbea9500d6d46c455480930bba6b3a0
Yeni Atencio <yeni.atencio@dpc.vic.gov.au>
2023-09-13 22:40:44 +1000

    Added reference to MFA tide core

--- dev-tools.sh
+++ dev-tools.sh
@@ -34,5 +34,6 @@
 #
 # Uncomment and set the Dev-Tools's commit value and commit this change.
 # export GH_COMMIT=COMMIT_SHA
+export GH_COMMIT=9cb8eb4a916efbc02e4b051672f0cec28925b37c
 
 bash <(curl -L https://raw.githubusercontent.com/dpc-sdp/dev-tools/master/install?"$(date +%s)") "$@"
